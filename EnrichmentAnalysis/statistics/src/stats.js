15c73d21786caeb3bdb63567bd490635fd8ab09a
Jaden Diefenbaugh <thevulch@users.noreply.github.com>
2015-10-13 21:11:13 -0700

    added initial stats package

---
+++ EnrichmentAnalysis/statistics/src/stats.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let stats = require('mathjs');
+let chisquared = require('chi-squared');
+
+function hypergeometric(k,K,n,N){
+    return null;
+}
+
+function fisher(k,K,n,N){
+    return null;
+}
+
+function chi(k,K,n,N){
+    return null;
+}
+
+module.exports = {
+    hypergeometric : hypergeometric,
+    fisher : fisher,
+    chi : chi
+};
