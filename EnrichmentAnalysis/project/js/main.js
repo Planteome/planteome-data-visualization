fd6c3d8fce7076c30e291d32374246a9429cf108
Qu Botong <qub@onid.oregonstate.edu>
2016-02-13 15:38:10 -0800

    query from server

--- EnrichmentAnalysis/project/js/main.js
+++ EnrichmentAnalysis/project/js/main.js
@@ -1,28 +1,246 @@
 'use strict';
 
+function taxon(name, id){
+	this.taxonId = id;
+	this.taxonName = name;
+}
+
+function initialize(){
+	
+	//initialize hints
+	$("#statistic_methods_hint p").html("When the input list is compared with the previously computed background,"+
+										"or is a subset of reference list, choose hypergeometric or fisher, "+
+										"for latter only when your query number is quite small. When the input" +
+										"list has few or no intersections with the reference list, the Chi-square" +
+										"tests are more appropriate."
+										);
+	$("#interesting_genes_hint p").html("Please make sure your request in the format that contains two rows. first row is ID, second row is GO accession.<br/>"+
+										"For example: <br/>"+
+										"test_0001	GO:0000001<br/>"+
+										"test_0002	GO:0000002<br/>"+
+										"or the accession ID of gene product. e.g.:<br/>TAIR:locus:2167042"
+										);	
+	$("#reference_background_hint p").html("please input the background reference data");
+		
+	//initialize the species selet button
+	var taxonList = [];
+	taxonList.push(new taxon("Arabidopsis_thaliana", 3702));
+	taxonList.push(new taxon('Aegilops_tauschii', '37682'));
+	taxonList.push(new taxon('Amborella_trichopoda', '13333'));
+	taxonList.push(new taxon('Arabidopsis_lyrata' , '59689'));
+	taxonList.push(new taxon('Arachis_duranensis', '130453'));
+	taxonList.push(new taxon('Arachis_ipaensis', '130454'));
+	
+	/*
+	'Aegilops_tauschii', '37682',
+	'Amborella_trichopoda', '13333',
+	'Arabidopsis_lyrata' , '59689',
+	'Arabidopsis_thaliana', '3702',
+	'Arachis_duranensis', '130453',
+	'Arachis_ipaensis', '130454',
+	
+	//
+	'Batrachochytrium_dendrobatidis', '109871',
+	'Brachypodium_distachyon', '15368',
+	'Brachypodium_sylvaticum.Corv', '29664',
+	'Brachypodium_sylvaticum.Greece', '29664',
+	'Brachypodium_sylvaticum.Spain', '29664',
+	'Brassica_oleracea', '3712',
+	'Brassica_rapa' ,'3711',
+	'Caenorhabditis_elegans', '6239',
+	'Cajanus_cajan', '3821',
+	'Cannabis_sativa.Purple.Kush', '3483',
+	'Capsella_rubella', '81985',
+	'Capsicum_annuum', '4072',
+	'Carica_papaya', '3649',
+	'Chlamydomonas_reinhardtii', '3055',
+	'Cicer_arietinum', '3827',
+	'Citrullus_lanatus', '3654',
+	'Citrus_clementina', '85681',
+	'Citrus_sinensis', '2711',
+	'Coffea_canephora', '49390',
+	'Cucumis_sativus', '3659',
+	'Danio_rerio', '7955',
+	'Drosophila_melanogaster', '7227',
+	'Ectocarpus_siliculosus', '2880',
+	'Elaphocordyceps_capitata', '45325',
+	'Elaphocordyceps_ophioglossoides', '71617',
+	'Elaphocordyceps_paradoxa', '94208',
+	'Epichloe_festucae', '35717',
+	'Escherichia_coli', '562',
+	'Eucalyptus_grandis', '71139',
+	'Fragaria_vesca', '57918',
+	'Fusarium_graminearum', '5518',
+	'Fusarium_oxysporum.4287', '660037',
+	'Fusarium_verticillioides', '117187',
+	'Gadus_morhua', '8049',
+	'Gasterosteus_aculeatus', '69293',
+	'Glycine_max', '3847',
+	'Gossypium_raimondii', '29730',
+	'Homo_sapiens', '9606',
+	'Hordeum_vulgare', '4513',
+	'Jatropha_curcas', '180498',
+	'Kalanchoe_laxiflora', '1670617',
+	'Laccaria_bicolor', '29883',
+	'Leersia_perrieri', '77586',
+	'Linum_usitatissimum', '4006',
+	'Magnaporthe_grissa', '148305',
+	'Malus_domestica', '3750',
+	'Manihot_esculenta', '3983',
+	'Medicago_truncatula', '3880',
+	'Mimulus_guttatus', '4155',
+	'Mus_musculus', '10090',
+	'Musa_acuminata', '4641',
+	'Nectria_haematococca', '140110',
+	'Nelumbo_nucifera', '4432',
+	'Neurospora_crassa', '5141',
+	'Nostoc_punctiforme', '272131',
+	'Oncorhynchus_mykiss', '8022',
+	'Oryza_australiensis', '4532',
+	'Oryza_barthii', '65489',
+	'Oryza_brachyantha', '4533',
+	'Oryza_glaberrima', '4538',
+	'Oryza_glumaepatula', '40148',
+	'Oryza_granulata', '110450',
+	'Oryza_kasalath', '4527',
+	'Oryza_longistaminata', '4528',
+	'Oryza_meridionalis', '40149',
+	'Oryza_minuta', '63629',
+	'Oryza_nivara', '4536',
+	'Oryza_officinalis', '4535',
+	'Oryza_punctata', '4537',
+	'Oryza_rufipogon', '4529',
+	'Oryza_sativa.indica.gramene', '39946',
+	'Oryza_sativa.indica.iplant', '39946',
+	'Oryza_sativa.indica.IR29', '39946',
+	'Oryza_sativa.indica.pokkali', '39946',
+	'Oryza_sativa.japonica.iplant', '39947',
+	'Oryza_sativa.japonica.IRGSP', '39947',
+	'Oryza_sativa.japonica.MSU', '39947',
+	'Oryzias_latipes', '8090',
+	'Pediculus_humanus', '121225',
+	'Phaseolus_vulgaris', '3885',
+	'Phoenix_dactylifera', '42345',
+	'Phyllostachys_heterocycla', '38705',
+	'Physcomitrella_patens', '3218',
+	'Phytophthora_infestans', '4787',
+	'Picea_abies', '3329',
+	'Pinus_taeda', '3352',
+	'Populus_trichocarpa', '3694',
+	'Populus_trichocarpa.ver3', '3694',
+	'Prunus_persica', '3760',
+	'Rattus_norvegicus', '10116',
+	'Rhizopus_oryzae', '64495',
+	'Ricinus_communis', '3988',
+	'Saccharomyces_cerevisiae', '4932',
+	'Salvia_hispancia.salba', '49212',
+	'Salvia_splendens', '180675',
+	'Schizosaccharomyces_pombe', '4896',
+	'Selaginella_moellendorffii', '88036',
+	'Setaria_italica', '4555',
+	'Solanum_lycopersicum', '4081',
+	'Solanum_tuberosum', '4113',
+	'Sorghum_bicolor', '4558',
+	'Synechocystis_pcc6803', '1148',
+	'Takifugu_rubripes', '31033',
+	'Tetraodon_nigroviridis', '99883',
+	'Theobroma_cacao', '3641',
+	'Tolypocladium_inflatum', '29910',
+	'Trichodesmium_erythraeum', '1206',
+	'Triticum_aestivum', '4565',
+	'Triticum_monococcum.DV92', '4568',
+	'Triticum_monococcum.G3116', '4568',
+	'Triticum_turgidum', '4571',
+	'Triticum_urartu', '4572',
+	'Vitis_vinifera', '29760',
+	'Zea_mays', '4577',
+	 */
+	var i;
+	for(i in taxonList){
+		$("#species_options").append($('<option/>', { 
+			value: taxonList[i].taxonId,
+			text : taxonList[i].taxonName
+		}));
+	}
+	
+	
+}
+
 function my_submit(){
 	var str_geneList= $("#textarea_geneList").val();
-	var str_referList = $("#textarea_backgroundList").val();
+	//var str_referList = $("#textarea_backgroundList").val();
 	if( str_geneList == ""){
 		alert("please input the interesting gene list");
 		return false;
 	}
-	if( str_referList == ""){
-		alert("please input the interesting gene list");
-		return false;
-	}
+	//if( str_referList == ""){
+	//	alert("please input the interesting gene list");
+	//	return false;
+	//}
 	
 	$("#result_table").html("<tr><th>GO term</th><th>Ontology</th><th>Description</th><th>Number in input list</th><th>Number in BG/Ref</th><th>p-value</th></tr>")
 	
+	/*
+	//static analysis
 	var inputAnnotationData = splitStringToAnnotation(str_geneList);
 	var referAnnotationData = splitStringToAnnotation(str_referList);
 	
 	$("#result_summary").html("the number of input genes is: "+inputAnnotationData.length+" <br> the number of background genes is: "+referAnnotationData.length+"<br>");
-	
-	var ontologyDataList = analyizeData(inputAnnotationData, referAnnotationData);
+		var ontologyDataList = staticAnalyizeData(inputAnnotationData, referAnnotationData);
+
 	var i;
 	for(i in ontologyDataList)
-		appendOntologyToRow(ontologyDataList[i]);
+		appendOntologyToRow(ontologyDataList[i]);		
+	*/
+	
+	// dynamic analysis	
+	var resultList = [];
+	var referenceGenesNum;									//N
+	var inputGenes = splitStringToGeneList(str_geneList);
+	var inputGenesNum = inputGenes.length;				//n
+	
+	$.when(getOverView(), getOntologyTermsFromGenes(inputGenes)).done(function(overview_data, ol_data){
+			
+		var summary = overview_data[0].summary;
+		referenceGenesNum = summary["gene-product-count"];
+		$("#result_summary").html("the number of input genes is: "+inputGenesNum+" <br> the number of background genes is: "+referenceGenesNum+"<br>");
+		//console.log("There are "+referenceGenesNum+" genes in database");
+		
+		//console.log(ol_data[0]);
+		//console.log("get the ontology term List " + ol_data[0].status);
+		var ontologyList = ol_data[0].summary["gene-to-term-summary-count"];
+		
+		$.when(getGenesNumInRefFromOntologys(ontologyList)).done(function(data, textStatus, jqXHR){
+			//console.log(data.summary);
+			var ontologyListRef = data.summary["term-to-gene-summary-count"];
+			
+			for(var i in ontologyList){
+				var ontoloy_ID = i;
+				
+				var numOfRefer = ontologyListRef[ontoloy_ID];	//K
+				var numOfInput = ontologyList[ontoloy_ID];		//k
+				var N = referenceGenesNum;						//N
+				var n = inputGenesNum;							//n
+				
+				let test_chi = stats.chi(numOfInput,numOfRefer,n,N);
+				var p = test_chi;
+				
+
+				
+				
+				var m_ontologyACC;
+				var m_description;
+				var m_ontologyData = new ontology(m_ontologyACC,ontoloy_ID, m_description, numOfInput, numOfRefer,p);
+				resultList.push(m_ontologyData);
+			}
+
+			//append to table
+			var i;
+			for(i in resultList)
+				appendOntologyToRow(resultList[i]);					
+		});
+
+	});
 	
 }
 
@@ -32,7 +250,7 @@ function my_reset(){
 	$("#result_table").html("<tr><th>GO term</th><th>Ontology</th><th>Description</th><th>Number in input list</th><th>Number in BG/Ref</th><th>p-value</th></tr>")
 }
 
-function analyizeData(inputData,referenceData){
+function staticAnalyizeData(inputData,referenceData){
 	var ontolotyIDList = [];
 	var i;
 	for(i in inputData){
@@ -59,7 +277,8 @@ function analyizeData(inputData,referenceData){
         console.log(`hyper:${test_hypergeo1} fisher:${test_fisher1} chi:${test_chi1}`);
 		
 		//choose stat method
-        var test_sel = $('#statistic_methods_input').children()[0].value;
+        //var test_sel = $('#statistic_methods_input').children()[0].value;
+		var test_sel = $('#statisticMethod').val();
         var p = '';
         switch(test_sel){
             case 'hypergeometric':
@@ -75,6 +294,10 @@ function analyizeData(inputData,referenceData){
                 console.error(`Invalid selection: ${test_sel}`);
                 break;
         }
+		
+		var cutoff = $('#significanceLevel').val();
+		if(p>cutoff)
+			continue;
 
 		var m_ontologyACC;
 		var m_description;
@@ -93,6 +316,80 @@ function getNumOfInput(str,inputData ){
 			num++;
 	}
 	return num;
+}
+
+//get the overview information from server
+function getOverView(){
+	//JS doesn't support function override
+    return $.ajax({
+		 type: "get",
+		 url: "http://test.planteome.org/api/overview",
+		 dataType: "json"
+	 });
+}
+
+function getOntologyTermsFromGenes(geneList){
+	
+	var link = "http://test.planteome.org:8080/gene-to-term?";
+	for(var i in geneList){
+		link +="q="+geneList[i]+"&";
+	}
+	link+="s=NCBITaxon:3702";
+	
+	//console.log(link);
+	
+	return $.ajax({
+		 type: "get",
+		 url: "http://test.planteome.org:8080/gene-to-term?q=TAIR:locus:2143261&s=NCBITaxon:3702",
+		 //url: link,
+		 dataType: "json"
+	});
+	
+	
+	/*
+	$.ajax({
+		 type: "get",
+		 url: "http://test.planteome.org:8080/gene-to-term?q=TAIR:locus:2143261&s=NCBITaxon:3702",
+		 dataType: "json",
+		 success: function(data){
+			console.log(data);
+			console.log("success");
+		 },
+		 error:function (){
+				alert("error");      
+		 }
+	});
+	
+	*/
+}
+
+function getGenesNumInRefFromOntologys(ontologyList){
+	
+	var link = "http://test.planteome.org/api/term-to-gene?s=NCBITaxon:3702";
+	for(var i in ontologyList){
+		link +="&q="+i;
+	}
+	
+	//console.log(link);
+	
+	return $.ajax({
+		 type: "get",
+		 //url: "http://test.planteome.org:8080/gene-to-term?q=TAIR:locus:2143261&s=NCBITaxon:3702",
+		 url: link,
+		 dataType: "json"
+	});
+}
+
+function getOntologyInfo(ontologyID){
+	
+	var link = "http://amigo.geneontology.org/amigo/term/GO:0022008/json";
+	console.log(link);
+	return $.ajax({
+		 type: "get",
+		 //url: "http://test.planteome.org:8080/gene-to-term?q=TAIR:locus:2143261&s=NCBITaxon:3702",
+		 url: link,
+		 dataType: "json"
+	});
 }
 
 function getNumOfRefer(str,referenceData){
@@ -153,6 +450,24 @@ function splitStringToAnnotation(str){
 	return annotationList;
 }
 
+function splitStringToGeneList(str){
+    
+	//var annotationList = [];
+	var geneIDList = [];
+	var inputData = str.split('\n');	// Split on carriage return
+	var x;
+	for(x in inputData){
+		var trimmedData = inputData[x].trim();
+		if(trimmedData ==="")
+			continue;
+ 		/*var txt = inputData[x].split(/\s+/g);
+		var annotationData = new annotation(txt[0], txt[1]);
+		annotationList.push(annotationData); */
+		
+		geneIDList.push(inputData[x]);	
+	}
+	return geneIDList;
+}
 /* function appendAnnotationToRow(obj){
 	var tr1 = document.createElement("tr");
 		
@@ -169,6 +484,8 @@ function appendOntologyToRow(obj){
 	var tr1 = document.createElement("tr");
 	var x;	
 	for (x in obj) {
+		
+		
 		var td = document.createElement("td");
  		var node = document.createTextNode(obj[x]);
 		td.appendChild(node);
